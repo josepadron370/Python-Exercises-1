47af82b0b663dc66de57e26667ede96b52fb46bc
josepadron370 <josepadron370@gmail.com>
2020-01-10 17:03:10 +0000

    First Commit

--- exercises/18-Random-Colors-Loop/app.py
+++ exercises/18-Random-Colors-Loop/app.py
@@ -4,24 +4,28 @@ def get_color(color_number=4):
     # making sure is a number and not a string
     color_number = int(color_number)
 
-    switcher={
+    pancakes={
                   0:'red',
                   1:'yellow',
                   2:'blue',
                   3:'green',
                   4:'black'
               }
-    return switcher.get(color_number,"Invalid Color Number")
+    return pancakes.get(color_number,"Invalid Color Number")
 
 
 def get_allStudentColors():
 
-    example_color = 1
+    # example_color = 1
     students_array = []
     #your loop here
 
+    for num in range(10):
+        num = get_color(random.randint(1,4))
+        students_array.append(num)
 
-
+    return students_array
+# print(get_allStudentColors())
 
 
 print(get_allStudentColors())
